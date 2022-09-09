e4f52202a75fcb1ef81684c1908fd5a84d15ec3c
jasonwarrick <jwarrick737@gmail.com>
2022-09-09 08:44:46 -0600

    Clickable tiles and smooth enemy movement

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float waitTime = 1f;
+    [SerializeField] [Range(0f, 5f)] float speed = 1f;
     
     // Start is called before the first frame update
     void Start() {
@@ -14,8 +14,17 @@ public class EnemyMover : MonoBehaviour
 
     IEnumerator FollowPath() { // IEnumerator makes the method a coroutine
         foreach(Waypoint waypoint in path) {
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(waitTime); // Returns a function that waits for one second, delaying each loop
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f; // Resets the travel counter so it always starts from 0
+
+            transform.LookAt(endPosition); // Rotates the enemy to face in its movement direction
+
+            while(travelPercent < 1f) {
+                travelPercent += Time.deltaTime * speed; // Makes travelPercent an incrementer for the time
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame(); // Restarts or breaks from the while loop after a frame is completed
+            }
         }
     }
 }
--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [SerializeField] bool isPlaceable;
+    
+    void OnMouseDown() {
+        if (Input.GetMouseButtonDown(0) && isPlaceable)  {
+            Debug.Log(transform.name);
+        }
     }
 }
